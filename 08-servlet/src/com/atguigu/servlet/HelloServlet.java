bd82e47977486ebf67dded424899c030fe349b1c
cnhkzyy <1069966476@qq.com>
2020-03-19 12:07:26 +0800

    更新HelloServlet类

--- 08-servlet/src/com/atguigu/servlet/HelloServlet.java
+++ 08-servlet/src/com/atguigu/servlet/HelloServlet.java
@@ -17,6 +17,14 @@ public class HelloServlet implements Servlet {
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
         System.out.println("2. init初始化方法");
+
+        //1. 可以获取Servlet程序的别名servlet-name的值
+        System.out.println("HelloServlet程序的别名是: " + servletConfig.getServletName());
+        //2. 获取初始化参数init-param
+        System.out.println("初始化参数username的值是: " + servletConfig.getInitParameter("username"));
+        System.out.println("初始化参数url的值是: " + servletConfig.getInitParameter("url"));
+        //3. 获取ServletContext对象
+        System.out.println(servletConfig.getServletContext());
     }
 
     @Override
