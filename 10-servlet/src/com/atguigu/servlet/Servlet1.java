12decdf0eccf9b5c7515871b120083150bb2f995
cnhkzyy <1069966476@qq.com>
2020-03-19 21:15:13 +0800

    新增Servlet1和Servlet2类

---
+++ 10-servlet/src/com/atguigu/servlet/Servlet1.java
@@ -0,0 +1,34 @@
+package com.atguigu.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Author beck
+ * Date 2020/3/19 20:52
+ **/
+public class Servlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取请求的参数(办事的材料)查看
+        String username = req.getParameter("username");
+        System.out.println("在Servlet1(柜台1)中查看参数(材料): " + username);
+
+        //给材料盖一个章并传递到Servlet2(柜台2)去查看
+        req.setAttribute("key1", "柜台1的章");
+
+        //问路：Servlet2(柜台2)怎么走
+        /**
+         * 请求转发必须要以斜杠打头，斜杠/ 表示地址为: http://ip:port/工程名，映射到IDEA代码的web目录
+         */
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/form.html");
+
+        //走向Servlet2(柜台2)
+        requestDispatcher.forward(req, resp);
+    }
+}
