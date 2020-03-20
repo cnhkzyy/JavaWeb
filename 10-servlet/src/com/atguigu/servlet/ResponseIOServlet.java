e041e02220f842c1b0855d5e2a2d64a7b27e402d
cnhkzyy <1069966476@qq.com>
2020-03-20 10:16:44 +0800

    更新ResponseIOServlet类

--- 10-servlet/src/com/atguigu/servlet/ResponseIOServlet.java
+++ 10-servlet/src/com/atguigu/servlet/ResponseIOServlet.java
@@ -18,8 +18,19 @@ public class ResponseIOServlet extends HttpServlet {
 //        resp.getWriter();
 //        resp.getOutputStream();
 
+        //默认字符集ISO-8859-1
+        System.out.println(resp.getCharacterEncoding());
+//        //设置服务器字符集为UTF-8
+//        resp.setCharacterEncoding("UTF-8");
+//        //通过响应头，设置浏览器也使用UTF-8字符集
+//        resp.setHeader("Content-Type", "text/html; charset=UTF-8");
+
+        //它会同时设置服务器和客户端都使用UTF-8字符集，还设置了响应头
+        //此方法一定要获取流对象之前调用才有效
+        resp.setContentType("text/html; charset=UTF-8");
+        System.out.println(resp.getCharacterEncoding());
         //要求：往客户端回传字符串数据
         PrintWriter writer = resp.getWriter();
-        writer.write("response's content");
+        writer.write("哈哈哈哈哈");
     }
 }
