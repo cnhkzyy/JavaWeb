43cb82200adc76d29d5a09bceeb0d559b6a338b0
cnhkzyy <1069966476@qq.com>
2020-03-20 10:01:08 +0800

    更新ResponseIOServlet类

--- 10-servlet/src/com/atguigu/servlet/ResponseIOServlet.java
+++ 10-servlet/src/com/atguigu/servlet/ResponseIOServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Author beck
@@ -14,7 +15,11 @@ public class ResponseIOServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //会报错，两个流只能拿一个
-        resp.getWriter();
-        resp.getOutputStream();
+//        resp.getWriter();
+//        resp.getOutputStream();
+
+        //要求：往客户端回传字符串数据
+        PrintWriter writer = resp.getWriter();
+        writer.write("response's content");
     }
 }
